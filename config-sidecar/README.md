437cd5587e5a19cb507306d5f3ec97ad204a3c9a
melancholiai <samit2310@gmail.com>
2022-08-01 18:44:59 +0300

    refactor: logging done with jslogger

--- config-sidecar/README.md
+++ config-sidecar/README.md
@@ -10,7 +10,7 @@ Usage: zx script.mjs [options]
 
 Options:
       --version          Show version number                           [boolean]
-  -i, --is-init-mode     The object storage endpoint   [boolean] [default: true]
+  -i, --is-init-mode     Is init mode flag             [boolean] [default: true]
   -e, --s3-endpoint-url  The object storage endpoint         [string] [required]
   -b, --s3-bucket-name   The bucket name containing all the keys
                                                              [string] [required]
@@ -22,22 +22,24 @@ Options:
   -u, --geoserver-url    the url to the reload endpoint on geoserver
                [string] [default: "http://localhost:8080/geoserver/rest/reload"]
   -D, --data-dir-path    The path to the data dir            [string] [required]
-  -I, --update-interval  The interval between configuration reload in milisecond
-                         s                            [number] [default: 120000]
-  -w, --wait-on-startup  The time to wait before starting in miliseconds
+  -I, --update-interval  The interval between configuration reload in millisecon
+                         ds                           [number] [default: 120000]
+  -w, --wait-on-startup  The time to wait before starting in milliseconds
                                                        [number] [default: 30000]
   -z, --zx-shell         The shell used by zx             [default: "/bin/bash"]
+  -l, --log-level        The log level
+          [choices: "debug", "info", "warn", "error", "fatal"] [default: "info"]
   -h, --help             Show help                                     [boolean]
 ```
 
-  
+
 ## Environment Variables
 
 Any option that can be set using the cli command line, can be also set by writing its value in `SNAKE_CASE`.
 
 For example, the option `--s3-bucket-name` can be set by using the `S3_BUCKET_NAME` environment variables.
 
-  
+
 ## Setting AWS SDK authentication
 
 In order to authenticate to Object storage, you need to supply the AWS credentials.
--- config-sidecar/script.mjs
+++ config-sidecar/script.mjs
@@ -1,11 +1,11 @@
 #!/usr/bin/env zx
-$.verbose = true;
 import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3';
 import * as stream from 'stream';
 import { once } from 'events';
 import * as util from 'util';
 import { join } from 'path';
 import yargs from 'yargs';
+import jsLogger from '@map-colonies/js-logger';
 
 const DATA_FILE = 'data.tar.gz';
 const REGION = 'us-east-1';
@@ -13,22 +13,39 @@ const REGION = 'us-east-1';
 const args = yargs(process.argv.slice(3))
   .env()
   .usage('Usage: $0 script.mjs [options]')
-  .option('is-init-mode', { alias: 'i', describe: 'The object storage endpoint', nargs: 1, boolean: true, default: true })
+  .option('is-init-mode', { alias: 'i', describe: 'Is init mode flag', nargs: 1, boolean: true, default: true })
   .option('s3-endpoint-url', { alias: 'e', describe: 'The object storage endpoint', nargs: 1, string: true, demandOption: true })
   .option('s3-bucket-name', { alias: 'b', describe: 'The bucket name containing all the keys', nargs: 1, string: true, demandOption: true })
   .option('s3-data-key', { alias: 'd', describe: 'The key for the data dir tar', nargs: 1, string: true, demandOption: true })
   .option('s3-state-key', { alias: 's', describe: 'The key for the config state object', nargs: 1, string: true, demandOption: true })
   .option('geoserver-user', { alias: 'U', describe: 'The geoserver user to authenticate with', nargs: 1, string: true })
   .option('geoserver-pass', { alias: 'P', describe: 'The geoserver password for the user to authenticate with', nargs: 1, string: true })
-  .option('geoserver-url', { alias: 'u', describe: 'the url to the reload endpoint on geoserver', default: 'http://localhost:8080/geoserver/rest/reload', nargs: 1, string: true })
+  .option('geoserver-url', {
+    alias: 'u',
+    describe: 'the url to the reload endpoint on geoserver',
+    default: 'http://localhost:8080/geoserver/rest/reload',
+    nargs: 1,
+    string: true,
+  })
   .option('data-dir-path', { alias: 'D', describe: 'The path to the data dir', nargs: 1, string: true, demandOption: true })
-  .option('update-interval', { alias: 'I', describe: 'The interval between configuration reload in milliseconds', nargs: 1, number: true, default: 120000 })
+  .option('update-interval', {
+    alias: 'I',
+    describe: 'The interval between configuration reload in milliseconds',
+    nargs: 1,
+    number: true,
+    default: 120000,
+  })
   .option('wait-on-startup', { alias: 'w', describe: 'The time to wait before starting in milliseconds', nargs: 1, number: true, default: 30000 })
   .option('zx-shell', { alias: 'z', describe: 'The shell used by zx', nargs: 1, default: '/bin/bash' })
+  .option('log-level', { alias: 'l', describe: 'The log level', choices: ['debug', 'info', 'warn', 'error', 'fatal'], default: 'info' })
   .help('h')
   .alias('h', 'help').argv;
 
+const logger = jsLogger.default({ level: args.logLevel });
+
 $.shell = args.zxShell;
+$.verbose = false;
+
 const currentStateFile = join(args.dataDirPath, 'state');
 
 const finished = util.promisify(stream.finished);
@@ -64,19 +81,24 @@ const s3Client = new S3Client({
 });
 
 const updateConfiguration = async () => {
+  logger.debug({ msg: 'getting state from storage', bucket: args.s3BucketName, key: args.s3StateKey });
+
   const remoteStateStream = await s3Client.send(new GetObjectCommand({ Bucket: args.s3BucketName, Key: args.s3StateKey }));
   const remoteState = await streamToString(remoteStateStream.Body);
 
-  if (!args.isInitMode && remoteState == (await $`cat ${currentStateFile}`)) {
-    console.log('state unchanged');
+  if (!args.isInitMode && remoteState == await $`cat ${currentStateFile}`) {
+    logger.info({ msg: 'state unchanged' });
     return;
   }
+
+  logger.debug({ msg: 'getting data from storage', bucket: args.s3BucketName, key: args.s3DataKey });
 
   const datadirStream = await s3Client.send(new GetObjectCommand({ Bucket: args.s3BucketName, Key: args.s3DataKey }));
 
   await streamToFile(datadirStream.Body, DATA_FILE);
 
-  console.log(chalk.blue('unpacking the data dir'));
+  logger.info({ msg: 'unpacking the data dir' });
+
   await $`tar -zxf ${DATA_FILE}`;
 
   await $`rsync -a --delete data_dir/* ${args.dataDirPath}`;
@@ -84,7 +106,8 @@ const updateConfiguration = async () => {
   await $`rm -rf ${DATA_FILE} && rm -rf data_dir/*`;
 
   if (!args.isInitMode) {
-    console.log(chalk.blue('sending config reload request to geoserver'));
+    logger.info({ msg: 'sending config reload request to geoserver' });
+
     const response = await fetch(args.geoserverUrl, {
       method: 'post',
       headers: {
@@ -93,25 +116,28 @@ const updateConfiguration = async () => {
     });
 
     if (!response.ok) {
-      console.log(chalk.red(`reloading geoserver configuration failed - ${response.statusText}`));
+      logger.error({ msg: 'reloading geoserver configuration failed', err: response.statusText });
       process.exit(1);
     }
   }
 
   await fs.writeFile(currentStateFile, remoteState);
 
-  console.log(`State updated to ${remoteState}`);
+  logger.info({ msg: 'state updated', updatedState: remoteState });
 };
 
 if (args.isInitMode) {
   await updateConfiguration();
-  console.log('finished updating, exiting gracefully');
+  logger.info({ msg: 'finished updating, exiting gracefully' });
 } else {
   // wait for geoserver to be ready
   await sleep(args.waitOnStartup);
 
   while (true) {
     await updateConfiguration();
+
+    logger.debug({ msg: 'sleeping for update interval', interval: args.updateInterval });
+
     await sleep(args.updateInterval);
   }
 }
