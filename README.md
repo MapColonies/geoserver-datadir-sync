1777d3a385930d7be364316c55f745a223bac59b
melancholiai <57397441+melancholiai@users.noreply.github.com>
2022-08-02 11:05:35 +0300

    refactor: logging improvements (#7)
    * refactor: logging done with jslogger
    
    * docs: geoserver application logging instructions
    
    * chore: added license
    
    * refactor: changes according to pr

--- README.md
+++ README.md
@@ -9,9 +9,15 @@ Because we want it to run on k8s, we added a sidecar that enables geoserver to b
 When a new geoserver pod is starting, an init-container that runs the image will download the datadir from S3 for the first time, so geoserver has an initial configuration.
 After the init container has finished it job, the geoserver image will start, and a sidecar that will check periodically for changes in S3, and if it detects one, it will download the new one, and apply it to the Geoserver image.
 
+## Logging
+- for custom geoserver logging define your logging schema and place it in `/data_dir/logs/custom_logging.xml` it is possible to log in json format using the `JsonTemplateLayout` [read more](https://logging.apache.org/log4j/2.x/manual/json-template-layout.html)
+
+- you can set the used logging schema through the geoserver UI under Global-Settings -> Logging Profile or specify it in `/data_dir/logging.xml`
+
+- in the UI you can also define your wanted requests logging strategy
+
 
 ## Installation
-
 set the values as you please, and run:
 ```bash
   cd helm
--- config-sidecar/script.mjs
+++ config-sidecar/script.mjs
@@ -1,11 +1,11 @@
 #!/usr/bin/env zx
-$.verbose = true;
 import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3';
 import * as stream from 'stream';
 import { once } from 'events';
 import * as util from 'util';
 import { join } from 'path';
 import yargs from 'yargs';
+import jsLogger from '@map-colonies/js-logger';
 
 const DATA_FILE = 'data.tar.gz';
 const REGION = 'us-east-1';
@@ -13,22 +13,39 @@ const REGION = 'us-east-1';
 const args = yargs(process.argv.slice(3))
   .env()
   .usage('Usage: $0 script.mjs [options]')
-  .option('is-init-mode', { alias: 'i', describe: 'The object storage endpoint', nargs: 1, boolean: true, default: true })
+  .option('is-init-mode', { alias: 'i', describe: 'Is init mode flag', nargs: 1, boolean: true, default: true })
   .option('s3-endpoint-url', { alias: 'e', describe: 'The object storage endpoint', nargs: 1, string: true, demandOption: true })
   .option('s3-bucket-name', { alias: 'b', describe: 'The bucket name containing all the keys', nargs: 1, string: true, demandOption: true })
   .option('s3-data-key', { alias: 'd', describe: 'The key for the data dir tar', nargs: 1, string: true, demandOption: true })
   .option('s3-state-key', { alias: 's', describe: 'The key for the config state object', nargs: 1, string: true, demandOption: true })
   .option('geoserver-user', { alias: 'U', describe: 'The geoserver user to authenticate with', nargs: 1, string: true })
   .option('geoserver-pass', { alias: 'P', describe: 'The geoserver password for the user to authenticate with', nargs: 1, string: true })
-  .option('geoserver-url', { alias: 'u', describe: 'the url to the reload endpoint on geoserver', default: 'http://localhost:8080/geoserver/rest/reload', nargs: 1, string: true })
+  .option('geoserver-url', {
+    alias: 'u',
+    describe: 'the url to the reload endpoint on geoserver',
+    default: 'http://localhost:8080/geoserver/rest/reload',
+    nargs: 1,
+    string: true,
+  })
   .option('data-dir-path', { alias: 'D', describe: 'The path to the data dir', nargs: 1, string: true, demandOption: true })
-  .option('update-interval', { alias: 'I', describe: 'The interval between configuration reload in milliseconds', nargs: 1, number: true, default: 120000 })
+  .option('update-interval', {
+    alias: 'I',
+    describe: 'The interval between configuration reload in milliseconds',
+    nargs: 1,
+    number: true,
+    default: 120000,
+  })
   .option('wait-on-startup', { alias: 'w', describe: 'The time to wait before starting in milliseconds', nargs: 1, number: true, default: 30000 })
   .option('zx-shell', { alias: 'z', describe: 'The shell used by zx', nargs: 1, default: '/bin/bash' })
+  .option('log-level', { alias: 'l', describe: 'The log level', choices: ['debug', 'info', 'warn', 'error', 'fatal'], default: 'info' })
   .help('h')
   .alias('h', 'help').argv;
 
+const logger = jsLogger.default({ level: args.logLevel });
+
 $.shell = args.zxShell;
+$.verbose = false;
+
 const currentStateFile = join(args.dataDirPath, 'state');
 
 const finished = util.promisify(stream.finished);
@@ -64,54 +81,70 @@ const s3Client = new S3Client({
 });
 
 const updateConfiguration = async () => {
-  const remoteStateStream = await s3Client.send(new GetObjectCommand({ Bucket: args.s3BucketName, Key: args.s3StateKey }));
-  const remoteState = await streamToString(remoteStateStream.Body);
+  try {
+    logger.debug({ msg: 'getting state from storage', bucket: args.s3BucketName, key: args.s3StateKey });
 
-  if (!args.isInitMode && remoteState == (await $`cat ${currentStateFile}`)) {
-    console.log('state unchanged');
-    return;
-  }
+    const remoteStateStream = await s3Client.send(new GetObjectCommand({ Bucket: args.s3BucketName, Key: args.s3StateKey }));
+    const remoteState = await streamToString(remoteStateStream.Body);
 
-  const datadirStream = await s3Client.send(new GetObjectCommand({ Bucket: args.s3BucketName, Key: args.s3DataKey }));
+    if (!args.isInitMode && remoteState == (await $`cat ${currentStateFile}`)) {
+      logger.info({ msg: 'state unchanged' });
+      return;
+    }
 
-  await streamToFile(datadirStream.Body, DATA_FILE);
+    logger.debug({ msg: 'getting data from storage', bucket: args.s3BucketName, key: args.s3DataKey });
 
-  console.log(chalk.blue('unpacking the data dir'));
-  await $`tar -zxf ${DATA_FILE}`;
+    const datadirStream = await s3Client.send(new GetObjectCommand({ Bucket: args.s3BucketName, Key: args.s3DataKey }));
 
-  await $`rsync -a --delete data_dir/* ${args.dataDirPath}`;
+    await streamToFile(datadirStream.Body, DATA_FILE);
 
-  await $`rm -rf ${DATA_FILE} && rm -rf data_dir/*`;
+    logger.info({ msg: 'unpacking the data dir' });
 
-  if (!args.isInitMode) {
-    console.log(chalk.blue('sending config reload request to geoserver'));
-    const response = await fetch(args.geoserverUrl, {
-      method: 'post',
-      headers: {
-        Authorization: 'Basic ' + Buffer.from(`${args.geoserverUser}:${args.geoserverPass}`).toString('base64'),
-      },
-    });
+    await $`tar -zxf ${DATA_FILE}`;
 
-    if (!response.ok) {
-      console.log(chalk.red(`reloading geoserver configuration failed - ${response.statusText}`));
+    await $`rsync -a --delete data_dir/* ${args.dataDirPath}`;
+
+    await $`rm -rf ${DATA_FILE} && rm -rf data_dir/*`;
+
+    if (!args.isInitMode) {
+      logger.info({ msg: 'sending config reload request to geoserver' });
+
+      const response = await fetch(args.geoserverUrl, {
+        method: 'post',
+        headers: {
+          Authorization: 'Basic ' + Buffer.from(`${args.geoserverUser}:${args.geoserverPass}`).toString('base64'),
+        },
+      });
+
+      if (!response.ok) {
+        logger.error({ msg: 'reloading geoserver configuration failed', err: response.statusText });
+        process.exit(1);
+      }
+    }
+
+    await fs.writeFile(currentStateFile, remoteState);
+    logger.info({ msg: 'state updated', updatedState: remoteState });
+  } catch (err) {
+    logger.error({ msg: 'an error occurred while updating configuration', err });
+
+    if (args.isInitMode) {
       process.exit(1);
     }
   }
-
-  await fs.writeFile(currentStateFile, remoteState);
-
-  console.log(`State updated to ${remoteState}`);
 };
 
 if (args.isInitMode) {
   await updateConfiguration();
-  console.log('finished updating, exiting gracefully');
+  logger.info({ msg: 'finished updating, exiting gracefully' });
 } else {
   // wait for geoserver to be ready
   await sleep(args.waitOnStartup);
 
   while (true) {
     await updateConfiguration();
+
+    logger.debug({ msg: 'sleeping for update interval', interval: args.updateInterval });
+
     await sleep(args.updateInterval);
   }
 }
